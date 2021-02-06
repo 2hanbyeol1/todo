802f748ebcebf6a50d0e24dde87491a9ce8e2b6a
한별 <78421872+2hanbyeol1@users.noreply.github.com>
2021-02-06 14:27:59 +0900

    Update how.js

--- js/how.js
+++ js/how.js
@@ -10,12 +10,10 @@ function setLanguage(event){
 	const lanBtn = event.target;
 	//한국어버튼
 	if(lanBtn.innerText === "한국어"){
-		console.log(lanBtn.innerText);
 		korContent.classList.add(SHOWING_CN);
 		engContent.classList.remove(SHOWING_CN);
 		lanBtn.innerText = "ENG";
 	} else {
-		console.log(lanBtn.innerText);
 		engContent.classList.add(SHOWING_CN);
 		korContent.classList.remove(SHOWING_CN);
 		lanBtn.innerText = "한국어";
