46574148119be54c32b6d36f471ae9292d9f1604
한별 <78421872+2hanbyeol1@users.noreply.github.com>
2021-02-20 15:10:07 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -9,6 +9,7 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 const TODOS_LS = "toDos";
 
 let toDos = [];
+let checkedNum = 0;
 let previousPercent = 0;
 
 function deleteNothing(){
@@ -19,214 +20,142 @@ function paintNothing(){
 	nothing.classList.add(SHOWING_CN);
 }
 
-function paintProgressBar(toDoLength, checkedNum){
-	let percent = 0;
-	if(toDos.length != 0) {
-		percent = Math.round(checkedNum / toDos.length * 100);
+function saveToDos(){
+	//localStorage only save as String type
+	//JSON(JS Object Notation)
+	localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+	deleteNothing();
+}
+
+function editToDo(event){
+	event.preventDefault();
+	let editForm;
+	if(event.target.className === "submitBtn"){
+		const submitBtn = event.target;
+		const editBtnspan = submitBtn.parentNode;
+		editForm = editBtnspan.parentNode;
+	} else {
+		editForm = event.target;
 	}
-	let width = previousPercent;
-	if(percent >= previousPercent){
-		const id = setInterval(function(){
-			if(width >= percent){
-				clearInterval(id);
-			} else {
-				width++;
-				toDoProgressBar.style.width = `${width}%`;
-				completed.innerText = `${width}% completed`;
-			}
-		}, 5);
-	} else{
-		const id = setInterval(function(){
-			if(width <= percent){
-				clearInterval(id);
-			} else {
-				width--;
-				toDoProgressBar.style.width = `${width}%`;
-				completed.innerText = `${width}% completed`;
-			}
-		}, 5);
+	const editInput = editForm.querySelector(".editInput");
+	const empty = editForm.querySelector(".empty");
+	const text = editInput.value;
+	if(text === ""){
+		//nothing written
+		empty.classList.add(SHOWING_CN);
+	} else {
+		//show edited to do list
+		empty.classList.remove(SHOWING_CN);
+		const li = editForm.parentNode;
+		editForm.removeChild(editInput);
+		li.removeChild(editForm);
+		const label = document.createElement("label");
+		const checkBox = document.createElement("input");
+		checkBox.setAttribute("type", "checkbox");
+		checkBox.setAttribute("class", "todoCheck");
+		const span = document.createElement("span");
+		span.setAttribute("class", "content");
+		const btnSpan = document.createElement("span");
+		btnSpan.setAttribute("class", "controlBtns");
+		const editBtn = document.createElement("img");
+		editBtn.setAttribute("class", "editBtn");
+		editBtn.src = "images/edit.png";
+		const delBtn = document.createElement("img");
+		delBtn.setAttribute("class", "delBtn");
+		delBtn.src = "images/delete.png";
+		checkBox.addEventListener("change", checkBoxChange);
+		editBtn.innerText = "edit";
+		editBtn.addEventListener("click", showEdit);
+		delBtn.innerText = "delete";
+		delBtn.addEventListener("click", deleteToDo);
+		span.innerText = `${text}`;
+		li.appendChild(label);
+		label.appendChild(checkBox);
+		label.appendChild(span);
+		li.appendChild(btnSpan);
+		btnSpan.appendChild(editBtn);
+		btnSpan.appendChild(delBtn);
+		//저장
+		for(var i = 0; i < toDos.length; i++){
+			if(toDos[i].id === parseInt(li.id)){
+				toDos[i].text = text;
+			};
+		}
+		saveToDos();
 	}
-	previousPercent = percent;
 }
 
 //edit undo
 function undo(event){
+	//hide editForm
+	//li > form > input, editBtns(undoBtn, submitBtn), editEmpty
 	const undoBtn = event.target;
-	const editBtnSpan = undoBtn.parentNode;
-	const editForm = editBtnSpan.parentNode;
-	const editInput = editForm.querySelector(".editInput");
-	const empty = editForm.querySelector(".empty");
-	empty.classList.remove(SHOWING_CN);
+	const editBtns = undoBtn.parentNode;
+	const editForm = editBtns.parentNode;
 	const li = editForm.parentNode;
-	editForm.removeChild(editInput);
+	const editInput = editForm.querySelector(".editInput");
+	const editEmpty = editForm.querySelector(".empty");
+	editEmpty.classList.remove(SHOWING_CN);
 	li.removeChild(editForm);
+	//show to do list (label, btns)
+	//li > label > input(checkbox), span(content)
 	const label = document.createElement("label");
 	const checkBox = document.createElement("input");
 	checkBox.setAttribute("type", "checkbox");
 	checkBox.setAttribute("class", "todoCheck");
-	const span = document.createElement("span");
-	span.setAttribute("class", "content");
-	const btnSpan = document.createElement("span");
-	btnSpan.setAttribute("class", "controlBtns");
+	const content = document.createElement("span");
+	content.setAttribute("class", "content");
+	//li > span(btns) > img(editBtn), img(delBtn)
+	const btns = document.createElement("span");
+	btns.setAttribute("class", "controlBtns");
 	const editBtn = document.createElement("img");
 	editBtn.setAttribute("class", "editBtn");
 	editBtn.src = "images/edit.png";
 	const delBtn = document.createElement("img");
 	delBtn.setAttribute("class", "delBtn");
 	delBtn.src = "images/delete.png";
+	//eventListener(checkbox, edit, delete)
+	checkBox.addEventListener("change", checkBoxChange);
+	editBtn.addEventListener("click", showEdit);
+	delBtn.addEventListener("click", deleteToDo);
 	for(var i = 0; i < toDos.length; i++){
 		if(toDos[i].id === parseInt(li.id)){
+			console.log(toDos[i].id);
+			console.log(li.id);
+			console.log(toDos[i].text);
 			checkBox.checked = toDos[i].isChecked;
 			const text = toDos[i].text;
-			span.innerText = `${text}`;
+			content.innerText = `${text}`;
 		};
 	}
-	saveToDos();
-	checkBox.addEventListener("change", checkBoxChange);
-	editBtn.innerText = "edit";
-	editBtn.addEventListener("click", showEdit);
-	delBtn.innerText = "delete";
-	delBtn.addEventListener("click", deleteToDo);
 	li.appendChild(label);
 	label.appendChild(checkBox);
-	label.appendChild(span);
-	li.appendChild(btnSpan);
-	btnSpan.appendChild(editBtn);
-	btnSpan.appendChild(delBtn);
-}
-
-//edit Input + btn
-function editToDoBtn(event){
-	event.preventDefault();
-	const submitBtn = event.target;
-	const editBtnspan = submitBtn.parentNode;
-	const editForm = editBtnspan.parentNode;
-	const editInput = editForm.querySelector(".editInput");
-	const empty = editForm.querySelector(".empty");
-	const text = editInput.value;
-	if(text === ""){
-		//nothing written
-		empty.classList.add(SHOWING_CN);
-	} else {
-		//show edited to do list
-		empty.classList.remove(SHOWING_CN);
-		const li = editForm.parentNode;
-		editForm.removeChild(editInput);
-		li.removeChild(editForm);
-		const label = document.createElement("label");
-		const checkBox = document.createElement("input");
-		checkBox.setAttribute("type", "checkbox");
-		checkBox.setAttribute("class", "todoCheck");
-		const span = document.createElement("span");
-		span.setAttribute("class", "content");
-		const btnSpan = document.createElement("span");
-		btnSpan.setAttribute("class", "controlBtns");
-		const editBtn = document.createElement("img");
-		editBtn.setAttribute("class", "editBtn");
-		editBtn.src = "images/edit.png";
-		const delBtn = document.createElement("img");
-		delBtn.setAttribute("class", "delBtn");
-		delBtn.src = "images/delete.png";
-		for(var i = 0; i < toDos.length; i++){
-			if(toDos[i].id === parseInt(li.id)){
-				checkBox.checked = toDos[i].isChecked;
-			};
-		}
-		checkBox.addEventListener("change", checkBoxChange);
-		editBtn.innerText = "edit";
-		editBtn.addEventListener("click", showEdit);
-		delBtn.innerText = "delete";
-		delBtn.addEventListener("click", deleteToDo);
-		span.innerText = `${text}`;
-		li.appendChild(label);
-		label.appendChild(checkBox);
-		label.appendChild(span);
-		li.appendChild(btnSpan);
-		btnSpan.appendChild(editBtn);
-		btnSpan.appendChild(delBtn);
-		
-		//localStorage save 'text'
-		for(var i = 0; i < toDos.length; i++){
-			if(toDos[i].id === parseInt(li.id)){
-				toDos[i].text = text;
-			};
-		}
-		saveToDos();
-	}
-}
-
-//edit Input + enter
-function editToDoEnter(event){
-	event.preventDefault();
-	const editForm = event.target;
-	const editInput = editForm.querySelector(".editInput");
-	const empty = editForm.querySelector(".empty");
-	const text = editInput.value;
-	if(text === ""){
-		//nothing written
-		empty.classList.add(SHOWING_CN);
-	} else {
-		//show edited to do list
-		empty.classList.remove(SHOWING_CN);
-		const li = editForm.parentNode;
-		editForm.removeChild(editInput);
-		li.removeChild(editForm);
-		const label = document.createElement("label");
-		const checkBox = document.createElement("input");
-		checkBox.setAttribute("type", "checkbox");
-		checkBox.setAttribute("class", "todoCheck");
-		const span = document.createElement("span");
-		span.setAttribute("class", "content");
-		const btnSpan = document.createElement("span");
-		btnSpan.setAttribute("class", "controlBtns");
-		const editBtn = document.createElement("img");
-		editBtn.setAttribute("class", "editBtn");
-		editBtn.src = "images/edit.png";
-		const delBtn = document.createElement("img");
-		delBtn.setAttribute("class", "delBtn");
-		delBtn.src = "images/delete.png";
-		checkBox.addEventListener("change", checkBoxChange);
-		editBtn.innerText = "edit";
-		editBtn.addEventListener("click", showEdit);
-		delBtn.innerText = "delete";
-		delBtn.addEventListener("click", deleteToDo);
-		span.innerText = `${text}`;
-		li.appendChild(label);
-		label.appendChild(checkBox);
-		label.appendChild(span);
-		li.appendChild(btnSpan);
-		btnSpan.appendChild(editBtn);
-		btnSpan.appendChild(delBtn);
-		
-		//localStorage save 'text'
-		for(var i = 0; i < toDos.length; i++){
-			if(toDos[i].id === parseInt(li.id)){
-				checkBox.checked = toDos[i].isChecked;
-				toDos[i].text = text;
-			};
-		}
-		saveToDos();
-	}
+	label.appendChild(content);
+	li.appendChild(btns);
+	btns.appendChild(editBtn);
+	btns.appendChild(delBtn);
 }
 
 function showEdit(event){
-	//hide to do list (label-checkbox, content, btns-edit,delete)
-	const btn = event.target;
-	const btnSpan = btn.parentNode;
-	const li = btnSpan.parentNode;
+	//hide to do list (label, btns)
+	//li > label(checkbox, content), btns(editBtn, delBtn)
+	const editBtn = event.target;
+	const btns = editBtn.parentNode;
+	const li = btns.parentNode;
 	const label = li.querySelector("label");
-	const span = li.querySelector(".content");
+	const content = li.querySelector(".content");
 	li.removeChild(label);
-	li.removeChild(btnSpan);
-	//show editForm (form, input, btn-undo, submit)
+	li.removeChild(btns);
+	//show editForm
+	//form > input, editBtns(undoBtn, submitBtn), editEmpty
 	const editForm = document.createElement("form");
 	const editInput = document.createElement("input");
 	editInput.setAttribute("type", "text");
 	editInput.setAttribute("class", "editInput");
 	editInput.setAttribute("placeholder", "EDIT YOUR TO DO!");
-	editInput.setAttribute("value", span.innerText);
-	const editbtnSpan = document.createElement("span");
-	editbtnSpan.setAttribute("class", "controlBtns");
+	editInput.setAttribute("value", content.innerText);
+	const editBtns = document.createElement("span");
+	editBtns.setAttribute("class", "controlBtns");
 	const undoBtn = document.createElement("img");
 	undoBtn.setAttribute("class", "undoBtn");
 	undoBtn.src = "images/undo.png";
@@ -238,43 +167,82 @@ function showEdit(event){
 	editEmpty.innerText = "You can't leave this empty."
 	li.appendChild(editForm);
 	editForm.appendChild(editInput);
-	editForm.appendChild(editbtnSpan);
-	editbtnSpan.appendChild(undoBtn);
-	editbtnSpan.appendChild(submitBtn);
+	editForm.appendChild(editBtns);
+	editBtns.appendChild(undoBtn);
+	editBtns.appendChild(submitBtn);
+	//eventListener(undo, submitBtn, submitEnter)
 	undoBtn.addEventListener("click", undo);
-	submitBtn.addEventListener("click", editToDoBtn);
+	submitBtn.addEventListener("click", editToDo);
 	editForm.appendChild(editEmpty);
-	editForm.addEventListener("submit", editToDoEnter);
+	editForm.addEventListener("submit", editToDo);
+}
+
+function paintProgressBar(checkedNum){
+	let percent = 0;
+	//퍼센트 계산: 체크 수 / to do 수 * 100 반올림
+	if(toDos.length != 0) {
+		percent = Math.round(checkedNum / toDos.length * 100);
+	}
+	//(처음)넓이: 이전 퍼센트
+	let width = previousPercent;
+	//퍼센트 증가
+	if(percent >= previousPercent){
+		const id = setInterval(function(){
+			//넓이가 퍼센트와 같거나 더 커졌을 때 > stop
+			if(width >= percent){
+				clearInterval(id);
+			//width 점점 늘어남
+			} else {
+				width++;
+				toDoProgressBar.style.width = `${width}%`;
+				completed.innerText = `${width}% completed`;
+			}
+		}, 5);
+	//퍼센트 감소
+	} else{
+		const id = setInterval(function(){
+			if(width <= percent){
+				clearInterval(id);
+			} else {
+				width--;
+				toDoProgressBar.style.width = `${width}%`;
+				completed.innerText = `${width}% completed`;
+			}
+		}, 5);
+	}
+	//이전 퍼센트가 됨
+	previousPercent = percent;
+}
+
+function selectCheckedNum(){
+	checkedNum = 0;
+	for(var i = 0; i < toDos.length; i++){
+		if(toDos[i].isChecked === true){
+			checkedNum = checkedNum + 1;
+		}
+	}
+	//프로그레스 바
+	paintProgressBar(checkedNum);
 }
 
 function deleteToDo(event){
+	//paint X
 	const btn = event.target;
 	const btnSpan = btn.parentNode;
 	const li = btnSpan.parentNode;
 	toDoList.removeChild(li);
+	//저장
 	const cleanToDos = toDos.filter(function(toDo){
 		return toDo.id !== parseInt(li.id);
 	});
 	toDos = cleanToDos;
 	saveToDos();
+	//nothing to do
 	if(toDos.length === 0){
 		paintNothing();
 	}
-	let checkedNum = 0;
-	for(var i = 0; i < toDos.length; i++){
-		//localStorage select the number of 'isChecked'
-		if(toDos[i].isChecked === true){
-			checkedNum = checkedNum + 1;
-		}
-	}
-	paintProgressBar(toDos.length, checkedNum);
-}
-
-function saveToDos(){
-	//localStorage only save as String type
-	//JSON(JS Object Notation)
-	localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
-	deleteNothing();
+	//체크수 조회 + 프로그레스 바
+	selectCheckedNum();
 }
 
 //checkbox clicked
@@ -282,55 +250,52 @@ function checkBoxChange(event){
 	const checkBox = event.target;
 	const label = checkBox.parentNode;
 	const li = label.parentNode;
-	let checkedNum = 0;
 	const isChecked = checkBox.checked;
+	//저장
 	for(var i = 0; i < toDos.length; i++){
-		//localStorage save 'checkBox.checked'
 		if(toDos[i].id === parseInt(li.id)){
 			toDos[i].isChecked = isChecked;
 		};
-		//localStorage select the number of 'isChecked'
-		if(toDos[i].isChecked === true){
-			checkedNum = checkedNum + 1;
-		}
 	}
 	saveToDos();
-	paintProgressBar(toDos.length, checkedNum);
+	//체크수 조회 + 프로그레스 바
+	selectCheckedNum();
 }
 
-//show to do
 function paintToDo(text, isChecked){
-	const newId = toDos.length + 1;
+	//li > label > input(checkbox), span(content)
 	const li = document.createElement("li");
 	const label = document.createElement("label");
 	const checkBox = document.createElement("input");
 	checkBox.setAttribute("type", "checkbox");
 	checkBox.setAttribute("class", "todoCheck");
 	checkBox.checked = isChecked;
-	const span = document.createElement("span");
-	span.setAttribute("class", "content");
-	const btnSpan = document.createElement("span");
-	btnSpan.setAttribute("class", "controlBtns");
+	const content = document.createElement("span");
+	content.setAttribute("class", "content");
+	//li > span(btns) > img(editBtn), img(delBtn)
+	const btns = document.createElement("span");
+	btns.setAttribute("class", "controlBtns");
 	const editBtn = document.createElement("img");
 	editBtn.setAttribute("class", "editBtn");
 	editBtn.src = "images/edit.png";
 	const delBtn = document.createElement("img");
 	delBtn.setAttribute("class", "delBtn");
 	delBtn.src = "images/delete.png";
+	//eventListener(checkbox, edit, delete)
 	checkBox.addEventListener("change", checkBoxChange);
-	editBtn.innerText = "edit";
 	editBtn.addEventListener("click", showEdit);
-	delBtn.innerText = "delete";
 	delBtn.addEventListener("click", deleteToDo);
-	span.innerText = `${text}`;
+	content.innerText = `${text}`;
 	li.appendChild(label);
 	label.appendChild(checkBox);
-	label.appendChild(span);
-	li.appendChild(btnSpan);
-	btnSpan.appendChild(editBtn);
-	btnSpan.appendChild(delBtn);
+	label.appendChild(content);
+	li.appendChild(btns);
+	btns.appendChild(editBtn);
+	btns.appendChild(delBtn);
+	const newId = toDos.length + 1;
 	li.id = newId;
 	toDoList.appendChild(li);
+	//저장
 	const toDoObj = {
 			text: text,
 			id: newId,
@@ -340,44 +305,40 @@ function paintToDo(text, isChecked){
 	saveToDos();
 }
 
-//to do Input + enter
+//to do 작성완료
 function toDoSubmit(event){
 	event.preventDefault();
 	const currentValue = toDoInput.value;
-	if(currentValue === ""){
+	if(currentValue === ""){ //공백확인
 		empty.classList.add(SHOWING_CN);
 	} else {
 		empty.classList.remove(SHOWING_CN);
+		//보여주기
 		paintToDo(currentValue, false);
-		let checkedNum = 0;
-		for(var i = 0; i < toDos.length; i++){
-			if(toDos[i].isChecked === true){
-				checkedNum = checkedNum + 1;
-			}
-		}
-		paintProgressBar(toDos.length, checkedNum);
+		//체크수 조회 + 프로그레스 바
+		selectCheckedNum();
 		toDoInput.value = "";
 	}
 }
 
 function loadToDos(){
 	const loadedToDos = localStorage.getItem(TODOS_LS);
-	let checkedNum = 0;
 	if(loadedToDos !== null){
+		//loadedToDos(String) → parsedToDos(Array)
 		const parsedToDos = JSON.parse(loadedToDos);
 		if(parsedToDos.length === 0){
+			//Nothing To Do
 			paintNothing();
 		} else {
-			parsedToDos.forEach(function(toDo){
-				paintToDo(toDo.text, toDo.isChecked);
-				//localStorage select the number of 'isChecked'
-				if(toDo.isChecked === true){
-					checkedNum = checkedNum + 1;
-				}
-			});
+			for(var i = 0; i < parsedToDos.length; i++){
+				//todo들을 보여줌
+				paintToDo(parsedToDos[i].text, parsedToDos[i].isChecked);
+			};
+			//체크수 조회 + 프로그레스 바
+			selectCheckedNum();
 		}
-		paintProgressBar(toDos.length, checkedNum);
 	} else {
+		//Nothing To Do
 		paintNothing();
 	}
 }
