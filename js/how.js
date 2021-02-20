c2892cdd3ed44420c5a7b87fccb86c7e2e16fee0
한별 <78421872+2hanbyeol1@users.noreply.github.com>
2021-02-20 15:10:30 +0900

    Update how.js

--- js/how.js
+++ js/how.js
@@ -1,22 +1,22 @@
-const settingBtn = document.querySelector(".js-how"),
+const howBtn = document.querySelector(".js-how"),
 	container = document.querySelector(".js-howToUse-container"),
 	exitBtn = document.querySelector(".js-exit"),
-	lanBtn = document.querySelector(".js-language"),
+	langBtn = document.querySelector(".js-language"),
 	engContent = document.querySelector(".js-howToUse-content-eng"),
 	korContent = document.querySelector(".js-howToUse-content-kor");
 	
 
 function setLanguage(event){
-	const lanBtn = event.target;
+	const langBtn = event.target;
 	//한국어버튼
-	if(lanBtn.innerText === "한국어"){
+	if(langBtn.innerText === "한국어"){
 		korContent.classList.add(SHOWING_CN);
 		engContent.classList.remove(SHOWING_CN);
-		lanBtn.innerText = "ENG";
+		langBtn.innerText = "ENG";
 	} else {
 		engContent.classList.add(SHOWING_CN);
 		korContent.classList.remove(SHOWING_CN);
-		lanBtn.innerText = "한국어";
+		langBtn.innerText = "한국어";
 	}
 }
 
@@ -29,9 +29,9 @@ function showHow(){
 }
 
 function init(){
-	settingBtn.addEventListener("click", showHow);
+	howBtn.addEventListener("click", showHow);
 	exitBtn.addEventListener("click", hideHow);
-	lanBtn.addEventListener("click", setLanguage);
+	langBtn.addEventListener("click", setLanguage);
 }
 
 init();
